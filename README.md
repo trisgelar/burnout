f31760a529ca6c7110cb1d3ababc2c36c2c3e9d4
Trisna Gelar <trisgelar@gmail.com>
2021-07-03 07:25:32 +0700

    first init

---
+++ README.md
@@ -0,0 +1,85 @@
+# burnout streamlit interface
+
+
+## Install pyenv & pipenv
+```sh
+	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zprofile
+	echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zprofile
+	echo 'eval "$(pyenv init --path)"' >> ~/.zprofile
+	
+	echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+	
+	$ git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+	echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+	
+	
+```
+
+## Set Env
+```sh
+	#pipenv --python 3.8.2 --verbose install
+	pipenv --python 3.8.7 --verbose install
+	
+	. ~/.local/share/virtualenvs/burnout-kH9yOvj2-python/bin/activate
+	make deps/update
+```
+
+## Install models
+
+```sh
+	export PYTHONPATH=/home/balaplumpat/Projects/Streamlit/burnout/tfod-api:/home/balaplumpat/Projects/Streamlit/burnout/tfod-api/research:/home/balaplumpat/Projects/Streamlit/burnout/tfod-api/research/slim
+
+	git clone -b b1d973e https://github.com/tensorflow/models.git
+	git clone -b b1d973ea103ee50d0933efe3c10136908b9909d9 https://github.com/tensorflow/models.git
+	git clone https://github.com/tensorflow/models tfod_api
+
+```
+
+```sh
+sudo apt-get install protobuf-compiler
+
+cd tfod_api/research
+touch use_protobuf.py
+
+import os
+import sys
+args = sys.argv
+directory = args[1]
+protoc_path = args[2]
+for file in os.listdir(directory):
+    if file.endswith(".proto"):
+        os.system(protoc_path+" "+directory+"/"+file+" --python_out=.")
+
+python use_protobuf.py  ./object_detection/protos/ /usr/bin/protoc
+cp object_detection/packages/tf2/setup.py .
+
+
+python ./object_detection/builders/model_builder_tf2_test.py
+
+```
+
+## Execute Streamlit
+```sh
+export STREAMLIT_SERVER_PORT=8080
+streamlit run --server.port $STREAMLIT_SERVER_PORT app.py
+```
+
+## Streamlit timeout error
+
+ubah timeout dari 10 menjadi 100 line 438 pada virtualenvs/{burnout-kH9yOvj2-python}/lib/python3.8/site-packages/streamlit_webrtc/webrtc.py
+
+```python
+
+self, sdp, type_, timeout: Union[float, None] = 10.0
+
+self, sdp, type_, timeout: Union[float, None] = 100.0
+
+
+```
+
+## Streamlit Ngorx Google Collabs
+[link1](https://medium.com/@jcharistech/how-to-run-streamlit-apps-from-colab-29b969a1bdfc)
+[link2](https://gist.github.com/tuffacton/da5a9b42c0a2e9e355353689f93c84b3)
+[link3](https://gist.github.com/MrFCow/8e0b497755d1f9ff1cf330f4af800911#file-instagrammatch-removed-core-functions-ipynb)
+[link4](https://github.com/napoles-uach/streamlit_apps/blob/main/Streamlit_Colab/06_Streamlit__Colab_BrainTumor.ipynb)
---
+++ app.py
@@ -0,0 +1,402 @@
+import asyncio
+import logging
+import logging.handlers
+import queue
+import threading
+import urllib.request
+from pathlib import Path
+from typing import List, NamedTuple
+
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal  # type: ignore
+
+import av
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pydub
+import streamlit as st
+from aiortc.contrib.media import MediaPlayer
+import os
+import sys
+import time
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+print("TF version:", tf.__version__)
+if tf.config.list_physical_devices('GPU'):
+    print("GPU is available",'\n')
+    print(tf.config.list_physical_devices('GPU'))
+else:
+    print("GPU is not available", '\n')
+
+from streamlit_webrtc import (
+    AudioProcessorBase,
+    ClientSettings,
+    VideoProcessorBase,
+    WebRtcMode,
+    webrtc_streamer,
+)
+
+HERE = Path(__file__).parent
+
+logger = logging.getLogger(__name__)
+
+def run_inference_for_single_image(model, image):
+    image = np.asarray(image)
+    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+    input_tensor = tf.convert_to_tensor(image)
+    # The export expects a batch of images, so add an axis with `tf.newaxis`.
+    input_tensor = input_tensor[tf.newaxis, ...]
+
+    # Run inference
+    model_fn = model.signatures['serving_default']
+    output_dict = model_fn(input_tensor)
+
+    # All outputs are batches tensors.
+    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+    # We're only interested in the first num_detections.
+    num_detections = int(output_dict.pop('num_detections'))
+    output_dict = {key: value[0, :num_detections].numpy()
+                   for key, value in output_dict.items()}
+    output_dict['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
+
+    # Handle models with masks:
+    if 'detection_masks' in output_dict:
+        # Reframe the the bbox mask to the image size.
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            output_dict['detection_masks'], output_dict['detection_boxes'],
+            image.shape[0], image.shape[1])
+        detection_masks_reframed = tf.cast(detection_masks_reframed > 0.8,
+                                           tf.uint8)
+        print(detection_masks_reframed)
+        output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
+
+    return output_dict
+
+# This code is based on https://github.com/streamlit/demo-self-driving/blob/230245391f2dda0cb464008195a470751c01770b/streamlit_app.py#L48  # noqa: E501
+def download_file(url, download_to: Path, expected_size=None):
+    # Don't download the file twice.
+    # (If possible, verify the download using the file length.)
+    if download_to.exists():
+        if expected_size:
+            if download_to.stat().st_size == expected_size:
+                return
+        else:
+            st.info(f"{url} is already downloaded.")
+            if not st.button("Download again?"):
+                return
+
+    download_to.parent.mkdir(parents=True, exist_ok=True)
+
+    # These are handles to two visual elements to animate.
+    weights_warning, progress_bar = None, None
+    try:
+        weights_warning = st.warning("Downloading %s..." % url)
+        progress_bar = st.progress(0)
+        with open(download_to, "wb") as output_file:
+            with urllib.request.urlopen(url) as response:
+                length = int(response.info()["Content-Length"])
+                counter = 0.0
+                MEGABYTES = 2.0 ** 20.0
+                while True:
+                    data = response.read(8192)
+                    if not data:
+                        break
+                    counter += len(data)
+                    output_file.write(data)
+
+                    # We perform animation by overwriting the elements.
+                    weights_warning.warning(
+                        "Downloading %s... (%6.2f/%6.2f MB)"
+                        % (url, counter / MEGABYTES, length / MEGABYTES)
+                    )
+                    progress_bar.progress(min(counter / length, 1.0))
+    # Finally, we remove these visual elements by calling .empty().
+    finally:
+        if weights_warning is not None:
+            weights_warning.empty()
+        if progress_bar is not None:
+            progress_bar.empty()
+
+
+WEBRTC_CLIENT_SETTINGS = ClientSettings(
+    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+    media_stream_constraints={
+        "video": True,
+        "audio": True,
+    },
+)
+
+
+def main():
+    st.header("Realtime Burnout Detection Application")
+
+    burnout_detection_page = "Real time burnout detection (sendrecv)"
+    app_mode = st.sidebar.selectbox(
+        "Choose the app mode",
+        [
+            burnout_detection_page,
+        ],
+    )
+    st.subheader(app_mode)
+
+    if app_mode == burnout_detection_page:
+        app_burnout_detection()
+
+    logger.debug("=== Alive threads ===")
+    for thread in threading.enumerate():
+        if thread.is_alive():
+            logger.debug(f"  {thread.name} ({thread.ident})")
+
+
+def app_object_detection():
+    """Object detection demo with MobileNet SSD.
+    This model and code are based on
+    https://github.com/robmarkcole/object-detection-app
+    """
+    MODEL_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.caffemodel"  # noqa: E501
+    MODEL_LOCAL_PATH = HERE / "./models/MobileNetSSD_deploy.caffemodel"
+    PROTOTXT_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.prototxt.txt"  # noqa: E501
+    PROTOTXT_LOCAL_PATH = HERE / "./models/MobileNetSSD_deploy.prototxt.txt"
+
+    CLASSES = [
+        "background",
+        "aeroplane",
+        "bicycle",
+        "bird",
+        "boat",
+        "bottle",
+        "bus",
+        "car",
+        "cat",
+        "chair",
+        "cow",
+        "diningtable",
+        "dog",
+        "horse",
+        "motorbike",
+        "person",
+        "pottedplant",
+        "sheep",
+        "sofa",
+        "train",
+        "tvmonitor",
+    ]
+    COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+    download_file(MODEL_URL, MODEL_LOCAL_PATH, expected_size=23147564)
+    download_file(PROTOTXT_URL, PROTOTXT_LOCAL_PATH, expected_size=29353)
+
+    DEFAULT_CONFIDENCE_THRESHOLD = 0.5
+
+    class Detection(NamedTuple):
+        name: str
+        prob: float
+
+    class MobileNetSSDVideoProcessor(VideoProcessorBase):
+        confidence_threshold: float
+        result_queue: "queue.Queue[List[Detection]]"
+
+        def __init__(self) -> None:
+            self._net = cv2.dnn.readNetFromCaffe(
+                str(PROTOTXT_LOCAL_PATH), str(MODEL_LOCAL_PATH)
+            )
+            self.confidence_threshold = DEFAULT_CONFIDENCE_THRESHOLD
+            self.result_queue = queue.Queue()
+
+        def _annotate_image(self, image, detections):
+            # loop over the detections
+            (h, w) = image.shape[:2]
+            result: List[Detection] = []
+            for i in np.arange(0, detections.shape[2]):
+                confidence = detections[0, 0, i, 2]
+
+                if confidence > self.confidence_threshold:
+                    # extract the index of the class label from the `detections`,
+                    # then compute the (x, y)-coordinates of the bounding box for
+                    # the object
+                    idx = int(detections[0, 0, i, 1])
+                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                    (startX, startY, endX, endY) = box.astype("int")
+
+                    name = CLASSES[idx]
+                    result.append(Detection(name=name, prob=float(confidence)))
+
+                    # display the prediction
+                    label = f"{name}: {round(confidence * 100, 2)}%"
+                    cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
+                    y = startY - 15 if startY - 15 > 15 else startY + 15
+                    cv2.putText(
+                        image,
+                        label,
+                        (startX, y),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5,
+                        COLORS[idx],
+                        2,
+                    )
+            return image, result
+
+        def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+            image = frame.to_ndarray(format="bgr24")
+            blob = cv2.dnn.blobFromImage(
+                cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
+            )
+            self._net.setInput(blob)
+            detections = self._net.forward()
+            annotated_image, result = self._annotate_image(image, detections)
+
+            # NOTE: This `recv` method is called in another thread,
+            # so it must be thread-safe.
+            self.result_queue.put(result)
+
+            return av.VideoFrame.from_ndarray(annotated_image, format="bgr24")
+
+    webrtc_ctx = webrtc_streamer(
+        key="object-detection",
+        mode=WebRtcMode.SENDRECV,
+        client_settings=WEBRTC_CLIENT_SETTINGS,
+        video_processor_factory=MobileNetSSDVideoProcessor,
+        async_processing=True,
+    )
+
+    confidence_threshold = st.slider(
+        "Confidence threshold", 0.0, 1.0, DEFAULT_CONFIDENCE_THRESHOLD, 0.05
+    )
+    if webrtc_ctx.video_processor:
+        webrtc_ctx.video_processor.confidence_threshold = confidence_threshold
+
+    if st.checkbox("Show the detected labels", value=True):
+        if webrtc_ctx.state.playing:
+            labels_placeholder = st.empty()
+            # NOTE: The video transformation with object detection and
+            # this loop displaying the result labels are running
+            # in different threads asynchronously.
+            # Then the rendered video frames and the labels displayed here
+            # are not strictly synchronized.
+            while True:
+                if webrtc_ctx.video_processor:
+                    try:
+                        result = webrtc_ctx.video_processor.result_queue.get(
+                            timeout=1.0
+                        )
+                    except queue.Empty:
+                        result = None
+                    labels_placeholder.table(result)
+                else:
+                    break
+
+    st.markdown(
+        "This demo uses a model and code from "
+        "https://github.com/robmarkcole/object-detection-app. "
+        "Many thanks to the project."
+    )
+
+def app_burnout_detection():
+    """ Media streamings """
+    MEDIAFILES = {
+        "test.mp4 (local)": {
+            "local_file_path": HERE / "data/test.mp4",
+            "type": "video",
+        },
+    }
+    media_file_label = "test.mp4 (local)"
+    media_file_info = MEDIAFILES[media_file_label]
+
+    PATH_TO_LABELS = os.path.join('models', 'labelmap.pbtxt')
+    PATH_TO_SAVED_MODEL = os.path.join('models', 'saved_model')
+
+    def create_player():
+        if "local_file_path" in media_file_info:
+            return MediaPlayer(str(media_file_info["local_file_path"]))
+
+        # NOTE: To stream the video from webcam, use the code below.
+        # return MediaPlayer(
+        #     "1:none",
+        #     format="avfoundation",
+        #     options={"framerate": "30", "video_size": "1280x720"},
+        # )
+
+    class OpenCVVideoProcessor(VideoProcessorBase):
+        type: Literal["noop", "burnout"]
+
+        def __init__(self) -> None:
+            self.type = "burnout"
+            self.category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+            self.detection_model = tf.saved_model.load(str(PATH_TO_SAVED_MODEL))
+
+
+        def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+            img = frame.to_ndarray(format="bgr24")
+
+            if self.type == "noop":
+                pass
+            elif self.type == "burnout":
+                output_dict = run_inference_for_single_image(self.detection_model, img)
+                img = vis_util.visualize_boxes_and_labels_on_image_array( img, 
+                    output_dict['detection_boxes'], 
+                    output_dict['detection_classes'],
+                    output_dict['detection_scores'],
+                    self.category_index,
+                    instance_masks=output_dict.get('detection_masks_reframed', None),
+                    use_normalized_coordinates=True,
+                    min_score_thresh=.5,
+                    line_thickness=8
+                )
+
+            return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+    WEBRTC_CLIENT_SETTINGS.update(
+        {
+            "media_stream_constraints": {
+                "video": media_file_info["type"] == "video",
+                "audio": media_file_info["type"] == "audio",
+            }
+        }
+    )
+
+    webrtc_ctx = webrtc_streamer(
+        key=f"media-streaming-{media_file_label}",
+        mode=WebRtcMode.RECVONLY,
+        client_settings=WEBRTC_CLIENT_SETTINGS,
+        player_factory=create_player,
+        video_processor_factory=OpenCVVideoProcessor,
+    )
+
+    if media_file_info["type"] == "video" and webrtc_ctx.video_processor:
+        webrtc_ctx.video_processor.type = st.radio(
+            "Select transform type", ("noop", "burnout")
+        )
+
+
+if __name__ == "__main__":
+    import os
+
+    DEBUG = os.environ.get("DEBUG", "false").lower() not in ["false", "no", "0"]
+
+    logging.basicConfig(
+        format="[%(asctime)s] %(levelname)7s from %(name)s in %(pathname)s:%(lineno)d: "
+        "%(message)s",
+        force=True,
+    )
+
+    logger.setLevel(level=logging.DEBUG if DEBUG else logging.INFO)
+
+    st_webrtc_logger = logging.getLogger("streamlit_webrtc")
+    st_webrtc_logger.setLevel(logging.DEBUG)
+
+    fsevents_logger = logging.getLogger("fsevents")
+    fsevents_logger.setLevel(logging.WARNING)
+
+    main()
